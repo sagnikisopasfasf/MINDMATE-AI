af69a060e4910d9010b7d71a8e9c5ea1b3bd32b5
sagnikisopasfasf <palsagnik1994@gmail.com>
2026-03-14 00:38:34 +0530

    latest commit

--- src/components/Starfield.jsx
+++ src/components/Starfield.jsx
@@ -69,9 +69,10 @@ const Starfield = () => {
         position: "fixed",
         top: 0,
         left: 0,
-        width: "100%",
-        height: "100%",
-        zIndex: -1,
+        width: "100vw",
+        height: "100vh",
+        zIndex: 0,
+        pointerEvents: "none",
         background: "black",
       }}
     />
